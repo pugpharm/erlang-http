8d259c4932c5aeed704f6295f941914ad6124edf
David Dossot <david@dossot.net>
2010-01-04 18:01:28 -0800

    added more handling for the new AMF representation of objects

--- src/http_mod_amf.erl
+++ src/http_mod_amf.erl
@@ -90,8 +90,14 @@ handle_amf_message(#amf_message{response = Response, body = Body}) ->
 -define(ERROR_MESSAGE,       <<"flex.messaging.messages.ErrorMessage">>).
 -define(ASYNC_MESSAGE,       <<"flex.messaging.messages.AsyncMessage">>).
 
+handle_amf_message_body([{avmplus, Msg}]) ->
+    handle_amf_message_body(Msg);
+
 handle_amf_message_body({avmplus, Msg}) ->
     handle_amf_message_body(Msg);
+
+handle_amf_message_body(Msg) when not is_list(Msg) ->
+    handle_amf_message_body([Msg]);
 
 handle_amf_message_body([{object, ?COMMAND_MESSAGE, Members} = Msg]) ->
     case proplists:get_value(operation, Members) of
